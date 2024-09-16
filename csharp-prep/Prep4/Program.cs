941c88c1fb478c73368496b5ede5491533224049
Nehikhare Efehi <nehikhareefehi@gmail.com>
2024-09-16 16:12:38 +0100

    A program that asks for numbers and append them to a list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.Ling;  // for sorting and list operations
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List>int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type o when finished. ");
+
+        // Loop to accept numbers until the user enters 0
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0)
+
+        // Core Requirement 1: Calculate sum
+        int sum = numbers.Sum();
+        Console.WriteLine($"the sum is: {sum}");
+
+        // Core Requirement 2: Calculate average
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        // Core Requirement 3: Find the largest number
+        int max = numbers.Max();
+        Console.WriteLine($"The ;argest number is: {max}");
+
+        //Stretch Challenge 1: Find the smallest positive number
+        int smallestPositive = numbers.Where(n => n > 0).Min();
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        //Stretch Challenge 2: Sort the list
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
