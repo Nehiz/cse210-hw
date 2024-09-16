3bdbbf5c4472e174abf1e3399e0f8231108d47ff
Nehikhare Efehi <nehikhareefehi@gmail.com>
2024-09-16 06:42:06 +0100

    A program that determines the letter grade for a course according to the grade percentage. Only core req. was meet at this stage.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Core Requirement: Ask student for the grade percentage
+        Console.WriteLine("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        // Initiatlize the letter grade variable
+        string letter = "";
+
+        // Core requirement 1: Series of if-else if-else statements to determine the letter grade based on percentage
+        // and print statement for each grade letter.
+        if (percentage >= 90)
+        {
+            // Console.WriteLine("Your grade is: A");
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+             // Console.WriteLine("Your grade is: B");
+            letter = "B";
+        }
+         else if (percentage >= 70)
+        {
+            // Console.WriteLine("Your grade is: C");
+            letter = "C";
+        }
+         else if (percentage >= 60)
+        {
+            // Console.WriteLine("Your grade is: D");
+            letter = "D";
+        }
+        else
+        {
+            // Console.WriteLine("Your grade is: F");
+            letter = "F";
+        }
+
+        // Core requirement 3: Print the letter grade once
+        Console.WriteLine($"Your grade is: {letter}");
+
+        //  Core req 2: Determine if the user passed or failed
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Work Harder! You'll get it next time.");
+        }
+
     }
 }
