4ca7699d874b9ad2d5159322ec27c5fa951d921a
Nehikhare Efehi <nehikhareefehi@gmail.com>
2024-09-16 08:09:47 +0100

    A program that collects grade percentage and output grade letter with the logic to add a "+"" or "-" to the letter grade.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Core Requirement: Ask student for the grade percentage
-        Console.WriteLine("Enter your grade percentage: ");
+        Console.Write("Enter your grade percentage: ");
         string input = Console.ReadLine();
         int percentage = int.Parse(input);
 
@@ -41,7 +41,9 @@ class Program
         }
 
         // Core requirement 3: Print the letter grade once
-        Console.WriteLine($"Your grade is: {letter}");
+       // Console.WriteLine($"Your grade is: {letter}");
+
+       Console.WriteLine();
 
         //  Core req 2: Determine if the user passed or failed
         if (percentage >= 70)
@@ -50,8 +52,41 @@ class Program
         }
         else
         {
-            Console.WriteLine("Work Harder! You can do it.");
+            Console.WriteLine("Work Harder! You can do this.");
         }
+
+        // Stretch challenge: 
+        // stretch 1: Add a "+" or "-" to the grade
+        string sign = "";
+        int lastDigit = percentage % 10;  // % operator is to get the remainder of the division (i.e the last digit of the numbers when they are divided by 10)
+
+        // Only add "+" or "-" for grades B, C, D
+        if (letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // Stretch 2: Special case for "A" grades, no A+ but allow A-
+        if (letter == "A" && lastDigit <3)
+        {
+            sign = "-";
+        }
+
+        // Stretch 3: Special case for "F" grades, no F+ or F-
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        // Print the final grade with letter and sign
+        Console.WriteLine($"Your grade is: {letter}{sign}");
 
     }
 }
